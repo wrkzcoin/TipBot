762776e342318fce1c0191dccba9cb4786f183b5
wrkzdev <40448869+wrkzdev@users.noreply.github.com>
2019-10-03 10:44:10 +0700

    balance update daemon to be running in endless loop

--- wrkzcoin_tipbot/bot_sql_update_balances.py
+++ wrkzcoin_tipbot/bot_sql_update_balances.py
@@ -7,104 +7,105 @@ import asyncio
 
 
 ENABLE_COIN = config.Enable_Coin.split(",")
-INTERVAL_EACH = 10
+INTERVAL_EACH = 30
 
 
 # Let's run balance update by a separate process
 async def update_balance():
-    print('sleep in second: '+str(INTERVAL_EACH))
-    # do not update yet
-    # XTOR:
-    COIN_NAME = "XTOR"
-    asyncio.sleep(INTERVAL_EACH)
-    print('Update balance: '+ COIN_NAME)
-    start = time.time()
-    try:
-        await store.sql_update_balances(COIN_NAME)
-    except Exception as e:
-        print(e)
-    end = time.time()
-    # End of XTOR
-    print('Done update balance: '+ COIN_NAME+ ' duration (s): '+str(end - start))
-    # LOKI:
-    COIN_NAME = "LOKI"
-    asyncio.sleep(INTERVAL_EACH)
-    print('Update balance: '+ COIN_NAME)
-    start = time.time()
-    try:
-        await store.sql_update_balances(COIN_NAME)
-    except Exception as e:
-        print(e)
-    end = time.time()
-    # End of LOKI
-    print('Done update balance: '+ COIN_NAME+ ' duration (s): '+str(end - start))
-    # XMR:
-    COIN_NAME = "XMR"
-    asyncio.sleep(INTERVAL_EACH)
-    print('Update balance: '+ COIN_NAME)
-    start = time.time()
-    try:
-        await store.sql_update_balances(COIN_NAME)
-    except Exception as e:
-        print(e)
-    end = time.time()
-    # End of XMR
-    print('Done update balance: '+ COIN_NAME+ ' duration (s): '+str(end - start))
-    # XEQ:
-    COIN_NAME = "XEQ"
-    asyncio.sleep(INTERVAL_EACH)
-    print('Update balance: '+ COIN_NAME)
-    start = time.time()
-    try:
-        await store.sql_update_balances(COIN_NAME)
-    except Exception as e:
-        print(e)
-    end = time.time()
-    # End of XEQ
-    # BLOG:
-    COIN_NAME = "BLOG"
-    asyncio.sleep(INTERVAL_EACH)
-    print('Update balance: '+ COIN_NAME)
-    start = time.time()
-    try:
-        await store.sql_update_balances(COIN_NAME)
-    except Exception as e:
-        print(e)
-    end = time.time()
-    # End of BLOG
-    # ARQ:
-    COIN_NAME = "ARQ"
-    asyncio.sleep(INTERVAL_EACH)
-    print('Update balance: '+ COIN_NAME)
-    start = time.time()
-    try:
-        await store.sql_update_balances(COIN_NAME)
-    except Exception as e:
-        print(e)
-    end = time.time()
-    # End of ARQ
-    # MSR:
-    COIN_NAME = "MSR"
-    asyncio.sleep(INTERVAL_EACH)
-    print('Update balance: '+ COIN_NAME)
-    start = time.time()
-    try:
-        await store.sql_update_balances(COIN_NAME)
-    except Exception as e:
-        print(e)
-    end = time.time()
-    # End of MSR
-    print('Done update balance: '+ COIN_NAME+ ' duration (s): '+str(end - start))
-    for coinItem in ENABLE_COIN:
+    while True:
+        print('sleep in second: '+str(INTERVAL_EACH))
+        # do not update yet
+        # XTOR:
+        COIN_NAME = "XTOR"
         asyncio.sleep(INTERVAL_EACH)
-        print('Update balance: '+ coinItem.upper().strip())
+        print('Update balance: '+ COIN_NAME)
         start = time.time()
         try:
-            await store.sql_update_balances(coinItem.upper().strip())
+            await store.sql_update_balances(COIN_NAME)
         except Exception as e:
             print(e)
         end = time.time()
-        print('Done update balance: '+ coinItem.upper().strip()+ ' duration (s): '+str(end - start))
+        # End of XTOR
+        print('Done update balance: '+ COIN_NAME+ ' duration (s): '+str(end - start))
+        # LOKI:
+        COIN_NAME = "LOKI"
+        asyncio.sleep(INTERVAL_EACH)
+        print('Update balance: '+ COIN_NAME)
+        start = time.time()
+        try:
+            await store.sql_update_balances(COIN_NAME)
+        except Exception as e:
+            print(e)
+        end = time.time()
+        # End of LOKI
+        print('Done update balance: '+ COIN_NAME+ ' duration (s): '+str(end - start))
+        # XMR:
+        COIN_NAME = "XMR"
+        asyncio.sleep(INTERVAL_EACH)
+        print('Update balance: '+ COIN_NAME)
+        start = time.time()
+        try:
+            await store.sql_update_balances(COIN_NAME)
+        except Exception as e:
+            print(e)
+        end = time.time()
+        # End of XMR
+        print('Done update balance: '+ COIN_NAME+ ' duration (s): '+str(end - start))
+        # XEQ:
+        COIN_NAME = "XEQ"
+        asyncio.sleep(INTERVAL_EACH)
+        print('Update balance: '+ COIN_NAME)
+        start = time.time()
+        try:
+            await store.sql_update_balances(COIN_NAME)
+        except Exception as e:
+            print(e)
+        end = time.time()
+        # End of XEQ
+        # BLOG:
+        COIN_NAME = "BLOG"
+        asyncio.sleep(INTERVAL_EACH)
+        print('Update balance: '+ COIN_NAME)
+        start = time.time()
+        try:
+            await store.sql_update_balances(COIN_NAME)
+        except Exception as e:
+            print(e)
+        end = time.time()
+        # End of BLOG
+        # ARQ:
+        COIN_NAME = "ARQ"
+        asyncio.sleep(INTERVAL_EACH)
+        print('Update balance: '+ COIN_NAME)
+        start = time.time()
+        try:
+            await store.sql_update_balances(COIN_NAME)
+        except Exception as e:
+            print(e)
+        end = time.time()
+        # End of ARQ
+        # MSR:
+        COIN_NAME = "MSR"
+        asyncio.sleep(INTERVAL_EACH)
+        print('Update balance: '+ COIN_NAME)
+        start = time.time()
+        try:
+            await store.sql_update_balances(COIN_NAME)
+        except Exception as e:
+            print(e)
+        end = time.time()
+        # End of MSR
+        print('Done update balance: '+ COIN_NAME+ ' duration (s): '+str(end - start))
+        for coinItem in ENABLE_COIN:
+            asyncio.sleep(INTERVAL_EACH)
+            print('Update balance: '+ coinItem.upper().strip())
+            start = time.time()
+            try:
+                await store.sql_update_balances(coinItem.upper().strip())
+            except Exception as e:
+                print(e)
+            end = time.time()
+            print('Done update balance: '+ coinItem.upper().strip()+ ' duration (s): '+str(end - start))
 
 loop = asyncio.get_event_loop()  
 loop.run_until_complete(update_balance())  

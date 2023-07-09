497f444c8c71feed5862eec71210d85e50ddc08a
wrkzdev <40448869+wrkzdev@users.noreply.github.com>
2023-07-09 08:16:39 +0700

    Fix text longer than 2000 for cexswap airdrop

--- wrkzcoin_tipbot/cogs/cexswap.py
+++ wrkzcoin_tipbot/cogs/cexswap.py
@@ -5676,8 +5676,8 @@ class Cexswap(commands.Cog):
                     random.shuffle(lp_discord_users)
                     lp_discord_users = lp_discord_users[0:max_alert]
                     msg = f"{EMOJI_INFORMATION} {ctx.author.mention}, successfully airdrop for pool: __{pool_name}__. Testing: __{testing}__"
-                    for each_u in lp_discord_users:
-                        list_receivers_str.append("UserID {}: {} {}".format(each_u, balance_user[str(each_u)], coin_name))
+                    for c, each_u in enumerate(lp_discord_users, start=1):
+                        list_receivers_str.append("{}) UserID {}: {} {}".format(c, each_u, balance_user[str(each_u)], coin_name))
                         if testing == "NO":
                             try:
                                 member = self.bot.get_user(int(each_u))
@@ -5694,8 +5694,20 @@ class Cexswap(commands.Cog):
                             except Exception:
                                 traceback.print_exc(file=sys.stdout)
                 msg += " And notified {} user(s).".format(num_notifying)
-                msg += "```" + "\n".join(list_receivers_str) + "```"
-                await ctx.edit_original_message(content=msg)
+                number_lines = 35
+                if len(list_receivers_str) <= number_lines:
+                    msg += "```" + "\n".join(list_receivers_str) + "```"
+                    await ctx.edit_original_message(content=msg)
+                else:
+                    await ctx.edit_original_message(content=msg)
+                    temp_lines =[]
+                    for c, i in enumerate(list_receivers_str, start=1):
+                        temp_lines.append(i)
+                        if c % number_lines == 0:
+                            await ctx.followup.send("```" + "\n".join(temp_lines) + "```")
+                            temp_lines = []
+                    if len(temp_lines) > 0:
+                        await ctx.followup.send("```" + "\n".join(temp_lines) + "```")
                 await log_to_channel(
                     "cexswap",
                     f"[AIRDROP LP]: User {ctx.author.mention} did airdrop LP for pools __{pool_name}__. Testing: {testing}!",
